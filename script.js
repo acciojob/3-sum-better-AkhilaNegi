c760ad09bb22306cc75cf182a9bba1c135494e50
Akhila Negi <122104289+AkhilaNegi@users.noreply.github.com>
2023-06-23 00:28:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function threeSum(arr, target) {
-// write your code here
-  
+	arr.sort((a, b) => a - b);
+	let closestSum = 0;
+	let minDiff = Number.MAX_VALUE;
+	for(let i=0; i<arr.length-2; i++){
+		let j=i+1, k = arr.length-1;
+		while(j < k){
+			const sum = (arr[i] + arr[j] + arr[k]);
+			if(sum > target) k--;
+			else j++;
+
+			const diff = Math.abs(sum - target);
+			if(diff < minDiff){
+				minDiff = diff;
+				closestSum = sum;
+			}
+		}
+	}
+	return closestSum;
 }
 
 module.exports = threeSum;
